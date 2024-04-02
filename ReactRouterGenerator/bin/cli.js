0dce241655d36aab59cdfd640fcf63c2fa6b58dc
Eshaan Joshi <eshaan@gmail.com>
2024-04-02 16:21:05 -0700

    initial file structure created. Optimized? for scalability in adding commands and generators

---
+++ ReactRouterGenerator/bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import {program} from 'commander'
+
+program
+	.name('react-router-generator')
+	.description('CLI tool to dynamically generate routes, data loaders, api and component')
+	.version('0.1.0')
