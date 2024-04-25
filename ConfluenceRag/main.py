f5a87ea17a4d979d5f7ee98b4c32ff02d6b2ef17
Eshaan Joshi <ejoshi@roambee.com>
2024-04-25 14:25:50 -0700

    configured a temp auth layer with placeholder network request in main.py. Updated .gitignore to ignore test cron job. To be impl at later date.

--- ConfluenceRag/main.py
+++ ConfluenceRag/main.py
@@ -1,9 +1,10 @@
-import streamlit as st
 import os
-
-from dotenv import load_dotenv
-from chatbot.my_llama_index import LlamaIndex
+import logging
 import requests
+import streamlit as st
+from dotenv import load_dotenv
+
+from chatbot.my_llama_index import LlamaIndex
 from requests.auth import HTTPBasicAuth
 from chatbot.confluence_api import (
     get_pages_for_space,
@@ -15,10 +16,9 @@ from chatbot.confluence_api import (
 
 from streamlit_keycloak import login
 
-import logging
 
 # Setup logging to capture stdout from the application
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
@@ -155,6 +155,7 @@ def authenticate_and_authorize():
 
 
 def main():
+    
     # keycloak = login(
     #     url=os.getenv("KC_URL"),
     #     realm=os.getenv("KC_REALM"),
@@ -162,155 +163,169 @@ def main():
     # )
 
     # if keycloak.authenticated:
+        # conf_client_id = os.getenv("CONFLUENCE_CLIENT_ID")
 
-    # conf_client_id = os.getenv("CONFLUENCE_CLIENT_ID")
+        # link_to_authenticate=f"""https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id={conf_client_id}&scope=read%3Apage%3Aconfluence%20read%3Aattachment%3Aconfluence%20read%3Aspace-details%3Aconfluence%20read%3Acontent-details%3Aconfluence%20read%3Acontent%3Aconfluence%20read%3Acustom-content%3Aconfluence%20read%3Aspace%3Aconfluence&redirect_uri=https%3A%2F%2Flocalhost%3A8501&state=$ragpath1234&response_type=code&prompt=consent
+        #         """
 
-    # link_to_authenticate=f"""https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id={conf_client_id}&scope=read%3Apage%3Aconfluence%20read%3Aattachment%3Aconfluence%20read%3Aspace-details%3Aconfluence%20read%3Acontent-details%3Aconfluence%20read%3Acontent%3Aconfluence%20read%3Acustom-content%3Aconfluence%20read%3Aspace%3Aconfluence&redirect_uri=https%3A%2F%2Flocalhost%3A8501&state=$ragpath1234&response_type=code&prompt=consent
-    #         """
+        # try:
+        #     conf_response = requests.get(
+        #         link_to_authenticate
+        #     )
+        #     conf_response.raise_for_status()
+        #     st.info(f"response from initial confluence request: {conf_response}")
+        #     processed_conf_response = conf_response.text
+        #     st.info(f"processed confluence response: {processed_conf_response}")
+        # except requests.exceptions.RequestException as e:
+        #     st.error(f"An error occurred making a confluence request for authentication: {e}")
 
-    # try:
-    #     conf_response = requests.get(
-    #         link_to_authenticate
-    #     )
-    #     conf_response.raise_for_status()
-    #     st.info(f"response from initial confluence request: {conf_response}")
-    #     processed_conf_response = conf_response.text
-    #     st.info(f"processed confluence response: {processed_conf_response}")
-    # except requests.exceptions.RequestException as e:
-    #     st.error(f"An error occurred making a confluence request for authentication: {e}")
+        # Configure the Streamlit app with pre-set settings
+        configure_streamlit_app()
 
-    # Configure the Streamlit app with pre-set settings
-    configure_streamlit_app()
+        info_placeholder = st.empty()
 
-    info_placeholder = st.empty()
+        # Authenticate and authorize the user
+        auth, headers, confluenceRestUrl = authenticate_and_authorize()
 
-    # Authenticate and authorize the user
-    auth, headers, confluenceRestUrl = authenticate_and_authorize()
+        # Ensure that the specified data directories exist
+        data_directory = "data/confluence_data"
+        list_spaces = ["RKB", "BH"]
+        directories_exist = ensure_data_directories(
+            data_directory, list_spaces, info_placeholder
+        )
 
-    # Ensure that the specified data directories exist
-    data_directory = "data/confluence_data"
-    list_spaces = ["RKB", "BH"]
-    directories_exist = ensure_data_directories(
-        data_directory, list_spaces, info_placeholder
-    )
-
-    # If necessary directories are not present, data is missing so fetch it
-    if not directories_exist:
-        print("data doesnt exist")
-        with st.spinner("Fetching and processing data..."):
-            try:
-                list_of_spaces = get_space_by_keys(
-                    confluenceRestUrl, headers, auth, list_spaces
-                )
-                for space in list_of_spaces:
-                    space_id = space.get("id")
-                    list_of_pages = get_pages_for_space(
-                        confluenceRestUrl, headers, auth, space_id
+        # If necessary directories are not present, data is missing so fetch it
+        if not directories_exist:
+            print("data doesnt exist")
+            with st.spinner("Fetching and processing data..."):
+                try:
+                    list_of_spaces = get_space_by_keys(
+                        confluenceRestUrl, headers, auth, list_spaces
                     )
-                    for page in list_of_pages:
-                        processed_text = process_page_content(page)
-                        save_processed_text_to_files(
-                            processed_text,
-                            space.get("key"),
-                            space.get("name"),
-                            page.get("title"),
-                            page.get("_links").get("webui"),
+                    for space in list_of_spaces:
+                        space_id = space.get("id")
+                        list_of_pages = get_pages_for_space(
+                            confluenceRestUrl, headers, auth, space_id
                         )
-            except requests.exceptions.RequestException as e:
-                st.error(f"An error occurred: {e}")
+                        for page in list_of_pages:
+                            processed_text = process_page_content(page)
+                            save_processed_text_to_files(
+                                processed_text,
+                                space.get("key"),
+                                space.get("name"),
+                                page.get("title"),
+                                page.get("_links").get("webui"),
+                            )
+                except requests.exceptions.RequestException as e:
+                    st.error(f"An error occurred: {e}")
 
-    info_placeholder.info("Data fetched and processed successfully. Loading Llama...")
+        info_placeholder.info("Data fetched and processed successfully. Loading Llama...")
 
-    # Set response streaming on or off
-    stream = True
+        # Set response streaming on or off
+        stream = True
 
-    # DEFAULT DON'T CHANGE
-    # SET TRUE FOR EXPIREMENTAL NODES
-    isNode = False
+        # DEFAULT DON'T CHANGE
+        # SET TRUE FOR EXPIREMENTAL NODES
+        isNode = False
 
-    # FOR DEV TESTING HTTP CLIENT:
-    isHTTP = True
+        # FOR DEV TESTING HTTP CLIENT:
+        isHTTP = True
 
-    # Initialize all LlamaIndex related things
-    llama_index = init_llama_index(data_directory, stream, isNode, isHTTP)
+        # Initialize all LlamaIndex related things
+        llama_index = init_llama_index(data_directory, stream, isNode, isHTTP)
 
-    info_placeholder.info(
-        "For more reading, check out [Roambee's Knowledge Base](https://roambee.atlassian.net/wiki/spaces/RKB/overview)"
-    )
+        info_placeholder.info(
+            "For more reading, check out [Roambee's Knowledge Base](https://roambee.atlassian.net/wiki/spaces/RKB/overview)"
+        )
 
-    # Initialize the chat message history
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            {
-                "role": "assistant",
-                "content": "Ask me anything about Roambee and our services!",
-            }
-        ]
+        # Initialize the chat message history
+        if "messages" not in st.session_state:
+            st.session_state.messages = [
+                {
+                    "role": "assistant",
+                    "content": "Ask me anything about Roambee and our services!",
+                }
+            ]
 
-    # Iterate over session_state.messages and display the chat history
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            # print(st.session_state.messages)
-            st.markdown(message["content"])
+        # Iterate over session_state.messages and display the chat history
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                # print(st.session_state.messages)
+                st.markdown(message["content"])
 
-    # Capture user input and save to chat history. Trigger llm query
-    if user_query := st.chat_input("Your query:"):
-        st.chat_message("user").markdown(user_query)
-        st.session_state.messages.append({"role": "user", "content": user_query})
+        # Capture user input and save to chat history. Trigger llm query
+        if user_query := st.chat_input("Your query:"):
+            st.chat_message("user").markdown(user_query)
+            st.session_state.messages.append({"role": "user", "content": user_query})
 
-        # Assigning this response to the assistant role
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                # Query the llama index to build a streamed response
-                response = llama_index.query(user_query)
-                # Initialize an empty placeholder for the displayed response
-                message_placeholder = st.empty()
-                # Initialize an empty string for the full response
-                full_response = ""
+            # Assigning this response to the assistant role
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    # Query the llama index to build a streamed response
+                    response = llama_index.query(user_query)
+                    # Initialize an empty placeholder for the displayed response
+                    message_placeholder = st.empty()
+                    # Initialize an empty string for the full response
+                    full_response = ""
 
-                # If stream was set True, capture the streamed tokens to build the response chunk-by-chunk in streamlits chat interface
-                # Else, render the full response in the chat interface
-                if stream:
-                    for chunk in response.response_gen:
-                        full_response += chunk
-                        message_placeholder.markdown(full_response + " ")
-                else:
-                    full_response = response.response
-                    message_placeholder.markdown(full_response)
+                    # If stream was set True, capture the streamed tokens to build the response chunk-by-chunk in streamlits chat interface
+                    # Else, render the full response in the chat interface
+                    if stream:
+                        for chunk in response.response_gen:
+                            full_response += chunk
+                            message_placeholder.markdown(full_response + " ")
+                    else:
+                        full_response = response.response
+                        message_placeholder.markdown(full_response)
 
-                additional_sources = "If you would like to know more, visit the following reference sources for more detailed information:\n"
+                    additional_sources = "If you would like to know more, visit the following reference sources for more detailed information:\n"
 
-                # Initialize an empty set to store unique confluence links
-                seen_links = set()
+                    # Initialize an empty set to store unique confluence links
+                    seen_links = set()
 
-                # Iterate over the source nodes in the response to capture confluence page links from node metadata
-                for node in response.source_nodes:
-                    webui_link = node.metadata.get("webui_link")
-                    if webui_link not in seen_links:
-                        additional_sources += f"- [{node.metadata.get('page_title')}]({webui_link})\n"
-                        seen_links.add(webui_link)
+                    # Iterate over the source nodes in the response to capture confluence page links from node metadata
+                    for node in response.source_nodes:
+                        webui_link = node.metadata.get("webui_link")
+                        if webui_link not in seen_links:
+                            additional_sources += f"- [{node.metadata.get('page_title')}]({webui_link})\n"
+                            seen_links.add(webui_link)
 
-                # append the additional sources string to the full response
-                full_response_with_links = full_response + "\n\n" + additional_sources
-                message_placeholder.markdown(full_response_with_links)
+                    # append the additional sources string to the full response
+                    full_response_with_links = full_response + "\n\n" + additional_sources
+                    message_placeholder.markdown(full_response_with_links)
 
-                # Add response to message history
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": full_response_with_links}
-                )
+                    # Add response to message history
+                    st.session_state.messages.append(
+                        {"role": "assistant", "content": full_response_with_links}
+                    )
 
-                # FOR DEV: Print relevant node information after response for inspection
-                for node in response.source_nodes:
-                    print(f"Page Title: {node.metadata.get('page_title')}")
-                    print(f"Score: {node.get_score()}")
-                    print(f"Metadata: {node.metadata}\n")
+                    # FOR DEV: Print relevant node information after response for inspection
+                    for node in response.source_nodes:
+                        print(f"Page Title: {node.metadata.get('page_title')}")
+                        print(f"Score: {node.get_score()}")
+                        print(f"Metadata: {node.metadata}\n")
 
-                    # Access extracted keywords
-                    keywords = node.metadata.get("excerpt_keywords")
+                        # Access extracted keywords
+                        keywords = node.metadata.get("excerpt_keywords")
 
-                    if keywords:
-                        print(f"Keywords: {keywords}")
+                        if keywords:
+                            print(f"Keywords: {keywords}")
+
+def auth_layer():
+    # a front request endpoint check for active session
+    session = requests.Session()
+
+    # REPLACE THIS URLWITH A NETWORK REQUEST TO THE USERS API
+    # OR CHECK FOR PRESENCE OF CERTAIN COOKIES
+    response = session.get(f"https://jsonplaceholder.typicode.com/todos/{1 + 1}")
+    print(f"FINDME: {response.json()}")
+
+    if response.ok:
+        print("User is authenticated")
+        main()
+    else:
+        print("User is not authenticated")
 
 
 if __name__ == "__main__":
-    main()
+    auth_layer()
